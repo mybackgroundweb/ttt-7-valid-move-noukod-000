95204da0f13e9630281f1b1c806bbbc681ae2aa9
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-04-05 12:55:44 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if (board[index] == " " && board[index]=="")
+  if (board[index] == " " || board[index]=="")
     token = true
   end
 end
