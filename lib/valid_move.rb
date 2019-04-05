489911bc4ee479846bdc91e639592f15091ae280
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-04-05 12:56:38 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,11 @@
 def valid_move?(board, index)
   if (board[index] == " " || board[index]=="")
     token = true
+  else
+    token = false
   end
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+  if 
